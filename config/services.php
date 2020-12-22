405fc34664d0471f52e801ee7c0750dbe04b5c82
ops <odox700@gmail.com>
2020-12-22 12:27:42 -0500

    Update services.php

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://backend.asbdev.com/receive-socialite-auth-code',
+        'redirect' => 'https://asbdev.com/receive-socialite-auth-code',
     ],
 
 ];
