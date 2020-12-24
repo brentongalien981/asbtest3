4936502d023eaa35f0cbed2dd7a254b769f3fa7e
ops <odox700@gmail.com>
2020-12-24 18:50:25 -0500

    current state

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::get('/getUserInfo', [TestLoginController::class, 'getUserInfo']);
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
